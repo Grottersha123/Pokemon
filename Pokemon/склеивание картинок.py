5a08dff2e97a591da34b1e6cef0c9ed9aaa80b73
Grottersha123 <grottersha123@gmail.com>
2016-08-25 20:56:55 +0300

    Нужно доделать эволюцию eevee  и сделать ввод с консоли

---
+++ Pokemon/склеивание картинок.py
@@ -0,0 +1,20 @@
+from PIL import Image
+def slpiting(pic):
+    p = 0
+    z = 0
+    x = 0
+    for i in pic:
+        im1 = Image.open(i+'.bmp')
+        x,y = im1.size
+        p += y
+    im = Image.new('RGB', (x, p))
+    for i in pic:
+        im1 = Image.open(i+'.bmp')
+        x,y = im1.size
+        im.paste(im1, (0,z))
+        z+= y
+    im.show()
+    
+    print(p,x)
+
+slpiting(['004'])
